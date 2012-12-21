3f8a17560179f6fa088477aaf0701c2665653ca0
Andrew Bell <cinder.afb@gmail.com>
2012-12-21 13:36:43 -0500

    Updating ocvCapture sample

--- samples/ocvCapture/src/ocvCaptureApp.cpp
+++ samples/ocvCapture/src/ocvCaptureApp.cpp
@@ -1,4 +1,4 @@
-#include "cinder/app/AppBasic.h"
+#include "cinder/app/AppNative.h"
 #include "cinder/gl/Texture.h"
 #include "cinder/Capture.h"
 
@@ -7,7 +7,7 @@
 using namespace ci;
 using namespace ci::app;
 
-class ocvCaptureApp : public AppBasic {
+class ocvCaptureApp : public AppNative {
   public:
 	void setup();
 	void update();
@@ -33,14 +33,11 @@ void ocvCaptureApp::update()
 	if( mCap && mCap.checkNewFrame() ) {
 		cv::Mat input( toOcv( mCap.getSurface() ) ), output;
 
-//		cv::threshold( input, output, 128, 255, CV_8U );
-		
 		cv::Sobel( input, output, CV_8U, 1, 0 );
-
+		
+//		cv::threshold( input, output, 128, 255, CV_8U );	
 //		cv::Laplacian( input, output, CV_8U );		
-
 //		cv::circle( output, toOcv( Vec2f(200, 200) ), 300, toOcv( Color( 0, 0.5f, 1 ) ), -1 );
-
 //		cv::line( output, cv::Point( 1, 1 ), cv::Point( 30, 30 ), toOcv( Color( 1, 0.5f, 0 ) ) );
 		
 		mTexture = gl::Texture( fromOcv( output ) );
@@ -55,4 +52,4 @@ void ocvCaptureApp::draw()
 }
 
 
-CINDER_APP_BASIC( ocvCaptureApp, RendererGl )
+CINDER_APP_NATIVE( ocvCaptureApp, RendererGl )
